d63077450ea8f48b6a597a14f2d50e5394135c8d
David <david.samah@student.moringaschool.com>
2025-09-25 13:01:27 +0300

    final

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// Write your code in this file!
-const currentUser="Grace Hopper";
-const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage=welcomeMessage.toUpperCase();
+const currentUser = 'Grace Hopper';
 
-const shortGreeting=`Welcome, ${currentUser[0]}!`
-console.log(shortGreeting);
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
